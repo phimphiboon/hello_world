680cc2bbd1506a7e5122878c1cb978e7e3b4f894
unknown <e61181020@gmail.com>
2021-03-06 15:47:37 +0700

    up

---
+++ lib/screen/home_screen.dart
@@ -0,0 +1,49 @@
+import 'package:flutter/material.dart';
+
+class HomeScreen extends StatefulWidget {
+  HomeScreen({Key key}) : super(key: key);
+
+  @override
+  _HomeScreenState createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Scaffold(
+        drawer: Drawer(
+          child: Text("Hello Drawer"),
+        ),
+        appBar: AppBar(
+          title: Text(
+            "Home Screen",
+            ),
+        ),
+        body: Center(
+          child: Container(
+          width: 150,
+          height: 150,
+          color: Colors.red,
+          child: Image.asset('assets/images/1.jpg'),
+        ),),
+        bottomNavigationBar: BottomNavigationBar(
+          items: [
+            BottomNavigationBarItem(
+              icon: Icon(Icons.home),
+              label: "Home",
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.phone),
+              label: "Phone",
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.dashboard_customize),
+              label: "Dashboard",
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
